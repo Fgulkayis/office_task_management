1890adc2a79efafcb37c12c03b2962c0cd2fc0e1
Fatma Gül Kayış <165270103+Fgulkayis@users.noreply.github.com>
2025-07-24 13:33:29 +0300

    Add Task model and File upload (MEDIA) settings

--- gorevler/admin.py
+++ gorevler/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Gorev, GorevDurumu
 
-# Register your models here.
+@admin.register(Gorev)
+class GorevAdmin(admin.ModelAdmin):
+    list_display = ('baslik', 'atanan_calisan', 'atan_yetkili', 'durum', 'olusturma_tarihi', 'baslama_tarihi', 'tamamlanma_tarihi',
+    'puan', 'get_tamamlanma_suresi',)
+    list_filter = ('durum', 'atanan_calisan', 'atan_yetkili', 'olusturma_tarihi')
+    search_fields = ('baslik', 'aciklama', 'atanan_calisan__username', 'atan_yetkili__username')
+    fieldsets = (
+        (None, {
+            'fields': ('baslik', 'aciklama', 'dosya',)
+        }),
+        ('Atama Bilgileri', {
+            'fields':('atan_yetkili', 'atanan_calisan')
+        }),
+        ('Durum ve Zaman Bilgileri', {
+            'fields':('durum', 'baslama_tarihi', 'tamamlanma_tarihi', 'puan')
+        }),
+        
+     )
+    readonly_fields = ('olusturma_tarihi', 'get_tamamlanma_suresi')
+    
+
+    def get_tamamlanma_suresi(self, obj):
+        if obj.tamamlanma_suresi:
+            total_seconds = int(obj.tamamlanma_suresi.total_seconds())
+            hours, remainder = divmod(total_seconds, 3600)
+            minutes, seconds = divmod(remainder, 60)
+            return f"{hours}s {minutes}dk {seconds}sn"
+        return "N/A"
+    get_tamamlanma_suresi.short_description = "Tamamlanma Süresi"
--- gorevler/models.py
+++ gorevler/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User 
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class GorevDurumu(models.TextChoices):
+    BASLATILMADI = 'BSL', 'Başlatılmadı'
+    DEVAM_EDIYOR = 'DVM', 'Devam Ediyor'
+    TAMAMLANDI = 'TMM', 'Tamamlandı'
+    IPTAL_EDILDI = 'IPT', 'İptal Edildi'
+
+class Gorev(models.Model):
+    baslik = models.CharField(max_length=200, verbose_name="Görev Başlığı")
+    aciklama = models.TextField(verbose_name="Görev Açıklaması")
+    atan_yetkili=models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='atanan_gorevler', verbose_name='Atayan Yetkili')
+    atanan_calisan = models.ForeignKey(User, on_delete=models.CASCADE, related_name='gorevlerim', verbose_name='Atanan Çalışan')
+    durum = models.CharField(max_length=3, choices=GorevDurumu.choices, default=GorevDurumu.BASLATILMADI, verbose_name='Görev Durumu')
+    olusturma_tarihi = models.DateTimeField(auto_now_add=True, verbose_name='Oluşturulma Tarihi')
+    baslama_tarihi = models.DateTimeField(null=True, blank=True, verbose_name='Başlama Tarihi')
+    tamamlanma_tarihi = models.DateTimeField(null=True, blank=True, verbose_name='Tamamlanma Tarihi')
+    dosya = models.FileField(upload_to='gorev_dosyalari/', null=True, blank=True, verbose_name="Ek Dosya")
+    puan = models.IntegerField(default=0, null=True, verbose_name="Görev Puanı", validators=[MinValueValidator(1), MaxValueValidator(5)])
+
+    class Meta:
+      ordering = ['-olusturma_tarihi']
+      verbose_name = 'Görev'
+      verbose_name_plural = "Görevler"
+
+
+    def __str__(self):
+       return f"{self.baslik} - {self.atanan_calisan.username} ({self.get_durum_display()})"
+
+    @property
+    def tamamlanma_suresi(self):
+      if self.baslama_tarihi and self.tamamlanma_tarihi:
+         return self.tamamlanma_tarihi - self.baslama_tarihi
+      return None
