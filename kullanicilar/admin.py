794006d35b4597506a1816640d2d7d5f30f0d238
Fatma Gül Kayış <165270103+Fgulkayis@users.noreply.github.com>
2025-07-24 10:42:16 +0300

    Add user models and integrate them into the admin panel

--- kullanicilar/admin.py
+++ kullanicilar/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import YetkiliProfil, CalisanProfil
 
-# Register your models here.
+class YetkiliProfilInline(admin.StackedInline):
+    model = YetkiliProfil
+    can_delete = False
+    verbose_name_plural = 'yetkili profil'
+
+
+class CalisanProfilInline(admin.StackedInline):
+    model = CalisanProfil
+    can_delete= False
+    verbose_name_plural='çalışan profil'
+
+class UserAdmin(BaseUserAdmin):
+    inlines =(YetkiliProfilInline, CalisanProfilInline)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
--- kullanicilar/models.py
+++ kullanicilar/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class YetkiliProfil(models.Model):
+    user= models.OneToOneField(User, on_delete= models.CASCADE, related_name='yetkili_profili')
+
+    def __str__(self):
+        return f"Yetkili: {self.user.username}"
+    
+
+class CalisanProfil(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='calisan_profil')
+    puan = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Çalışan: {self.user.username} (Puan: {self.puan})"
+    
+def is_yetkili(user):
+    return hasattr(user, 'yetkili_profil')
+
+def is_calisan(user):
+    return hasattr(user, 'calisan_profil')
+
+
